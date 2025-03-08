e94991ea2dc4b74bb53dc814556060d476ce53f6
dexpie <gadingputra103@gmail.com>
2025-03-08 15:42:15 +0700

    Ini bisa coy

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const galleryImages = [
     "https://i.pinimg.com/736x/0a/65/64/0a656443ea48ac6408ce034f8b28ee0c.jpg",
@@ -6,12 +6,45 @@ const galleryImages = [
     "https://i.pinimg.com/736x/4b/f2/99/4bf2994702ce7fbdeccacaa57eb19748.jpg"
 ];
 
-const Gallery = () => (
-    <div>
-        {galleryImages.map((image, index) => (
-            <img key={index} src={image} alt={`Image ${index + 1}`} />
-        ))}
-    </div>
-);
+const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const openModal = (image) => {
+        setSelectedImage(image);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+    };
+
+    return (
+        <section id="gallery" className="gallery-section">
+            <div className="container">
+                <h2 className="section-title">Our Gallery</h2>
+                <p className="section-description">Discover the highlights of our past events through our photo gallery.</p>
+
+                <div className="gallery-grid">
+                    {galleryImages.map((image, index) => (
+                        <div key={index} className="gallery-item">
+                            <img
+                                src={image}
+                                alt={`Himmel sang pahlawan`} // Simple dynamic alt for each image
+                                onClick={() => openModal(image)}
+                            />
+                        </div>
+                    ))}
+                </div>
+
+                {selectedImage && (
+                    <div className="modal" onClick={closeModal}>
+                        <div className="modal-content">
+                            <img src={selectedImage} alt="Selected" className="modal-image" />
+                        </div>
+                    </div>
+                )}
+            </div>
+        </section>
+    );
+};
 
 export default Gallery;
